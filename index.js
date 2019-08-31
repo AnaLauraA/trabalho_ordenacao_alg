e8674b2cb0fc49497029b6dc19a6ff10d1ef9de8
Saulo Andrade <sauloandrade0809@gmail.com>
2019-08-30 22:18:53 -0300

    Fiz as funções bubblesort, insercaoDireta e selecaoDireta

--- index.js
+++ index.js
@@ -20,3 +20,45 @@ function
 
 
 /----------------------------------------------------------------
+
+function bubblesort(vet){
+    for(var i = vet.length - 1; i >= 0; i--){
+        for(var j = 0; j < i; j++){
+            if(vet[j] > vet[j + 1]){
+                var aux = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
+            }
+        }
+    }
+}
+
+function selecaoDireta(vet){
+    for(var i = 0; i < vet.length - 1; i++){
+        var menor = vet[i];
+        var posMenor;
+        for(var j = i + 1; j < vet.length;j++){
+            if(vet[j] < menor){
+                menor = vet[j];
+                posMenor = j;
+            }
+        }
+        var aux = vet[i];
+        vet[i] = menor;
+        vet[posMenor] = aux;
+    }
+}
+
+function insercaoDireta(vet){
+    for(var i = 1; i < vet.length; i++){
+        for(var j = i; j >= 0; j--){
+            if(vet[j] < vet[j - 1]){
+                var aux = vet[j];
+                vet[j] = vet[j - 1];
+                vet[j - 1] = aux;
+                break;
+            }
+        }
+    }
+    return vet;
+}
